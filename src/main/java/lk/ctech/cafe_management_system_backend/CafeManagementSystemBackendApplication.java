95db73eb87135efd9e6285b68a2ea1eecf110763
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-06 10:18:34 +0530

    initial commit

---
+++ src/main/java/lk/ctech/cafe_management_system_backend/CafeManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ctech.cafe_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ctech/cafe_management_system_backend/CafeManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ctech.cafe_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
