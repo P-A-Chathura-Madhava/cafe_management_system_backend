1636933010cc7420bd769816ad6e9e85d3ec4dea
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-16 21:02:43 +0530

    enhance manage order

--- src/main/java/lk/ctech/cafe_management_system_backend/constents/CafeConstants.java
+++ src/main/java/lk/ctech/cafe_management_system_backend/constents/CafeConstants.java
@@ -4,5 +4,5 @@ public class CafeConstants {
     public static final String SOMETHING_WENT_WRONG = "Something went Wrong";
     public static final String INVALID_DATA = "Invalid Data";
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized access";
-    public static final String STORE_LOCATION = "C:\\Users\\Admin\\Desktop\\My Files\\Programming\\Projects\\Spring - Angular Cafe Management System Backend\\PDFs";
+    public static final String STORE_LOCATION = "C:\\Users\\Admin\\Desktop\\My Files\\Programming\\Projects\\My Stack\\Cafe Management System Spring - React\\frontend\\Frontend\\Frontend\\PDFs";
 }
